a8e175886da8ecd43adb42177ca4ca4451544e2b
Manjushree Berike Rajanna <manju_br@Manjushrees-MBP.attlocal.net>
2021-09-07 23:24:00 -0700

    Database connecton added

--- ubereats_back/app.js
+++ ubereats_back/app.js
@@ -1,9 +1,34 @@
 const express = require('express')
+const mysql = require('mysql')
 const app = express();
 require('dotenv').config();
 
-app.get('/',(req, res)=>{
-    res.send("Hello from node updated");
+//database connection
+var db = mysql.createConnection({
+    host: 'localhost',
+    user: 'admin',
+    password: 'X43Bfb).YYCwGLrP',
+    database: 'ubereatsDB'
+});
+//connect db
+db.connect((error) => {
+    if(error){
+        throw(error);
+    }
+    else{
+        console.log('Mysql DB connected');
+    }
+
+})
+
+//create table
+app.get('/restaurant',(req, res)=>{
+    let sql = "CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY (id))";
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log('posts table created ')
+    });
+  
 });
 
 const port = process.env.PORT || 8000
