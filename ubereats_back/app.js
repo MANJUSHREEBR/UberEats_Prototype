0bedf4bd18fb91e0159007d71b2e2a833420c4be
Manjushree Berike Rajanna <manju_br@Manjushrees-MBP.attlocal.net>
2021-09-07 23:24:46 -0700

    db connection added

--- ubereats_back/app.js
+++ ubereats_back/app.js
@@ -21,15 +21,6 @@ db.connect((error) => {
 
 })
 
-//create table
-app.get('/restaurant',(req, res)=>{
-    let sql = "CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY (id))";
-    db.query(sql, (err, result) => {
-        if(err) throw err;
-        console.log('posts table created ')
-    });
-  
-});
 
 const port = process.env.PORT || 8000
 
